3685eaccf4c1a7f2ea6cc8e42fa6f46c1d1b498d
Vadim Smagin <magenvadim@gmail.com>
2024-02-28 04:21:19 +0200

    #12 checkbox code refactoring

--- src/App.js
+++ src/App.js
@@ -19,62 +19,42 @@ function App() {
     {step: 3, info:"ADD-ONS"},
     {step: 4, info:"Summary"},
   ]
-
   const plan_priced = [
     {plan_name:"Arcade", plan_price:"$9/mo", img_source:IconArcade},
     {plan_name:"Advanced", plan_price:"$12/mo", img_source:IconAdvanced},
     {plan_name:"Pro", plan_price:"$15/mo", img_source:IconPro}
   ]
 
-  const add_ons = [
-    {service:"Online service", price:"+1/mo", checkStatus:false},
-    {service:"Larger storage", price:"+2/mo", checkStatus:false},
-    {service:"Customizable Profile", price:"+2/mo", checkStatus:false}
-  ]
-  const handleAddOnsChange = (e)=>{ 
-
-      // const nextCheckBoxStatus = checkBoxStatus.map((obj)=>{
-      //   if(obj.service===e.target.value){
-      //     return {...obj, checkStatus: true}
-      //   } else {
-      //     return obj
-      //   }
-      // })
-      // setCheckBoxStatus(nextCheckBoxStatus)
-
-      if(e.target.checked) {
-        add_ons.map((elem)=>{          
-          if(e.target.value===elem.service)
-          set_add_ons_selected([...add_ons_selected, elem]);        
-        });
-      } else { 
-        set_add_ons_selected(add_ons_selected.filter(obj=>obj.service != e.target.value))        
-      } 
-  }
-
-
-
-
   const [stepNumber, setStepNumber] = useState(1)
   const [nameValue, setNameValue] = useState('')
   const [emailValue, setEmailValue] = useState('')
   const [phoneValue, setPhoneValue] = useState('')
   const [selectedID, setSelectedID] = useState(0)
-  const [checkBoxStatus, setCheckBoxStatus] = useState(add_ons)
 
   const [add_ons_selected, set_add_ons_selected] = useState([
-    {service:"Online service", price:"+$1/mo", checkStatus:true},
+    {service:"Online service", price:"+$1/mo", checkStatus:false},
     {service:"Larger storage", price:"+$2/mo", checkStatus:true},
     {service:"Customizable Profile", price:"+$2/mo", checkStatus:false}
   ])
 
-  useEffect(()=>
-    add_ons_selected.forEach((elem)=>console.log(elem.service + ", " + elem.price)
-  ),[add_ons_selected])
-  // useEffect(()=>console.log(checkBoxStatus),[checkBoxStatus])
-
-  useEffect(()=>console.log(nameValue),
-  [stepNumber])
+  const handleAddOnsChange = (e)=>{
+    
+    if(e.target.checked) {      
+      const nextCheckBoxStatus = add_ons_selected.map((elem)=>{
+        if(e.target.value===elem.service) {
+          return {...elem, checkStatus: true}
+        } else return elem
+      })
+      set_add_ons_selected(nextCheckBoxStatus)
+    } else {
+      const nextCheckBoxStatus = add_ons_selected.map((elem)=>{
+        if(e.target.value===elem.service) {
+          return {...elem, checkStatus: false}
+        } else return elem
+      })
+      set_add_ons_selected(nextCheckBoxStatus)
+    }
+  }
 
   return (
     <div className="App">
@@ -115,7 +95,7 @@ function App() {
                   stepNumber={stepNumber} 
                   setStepNumber={setStepNumber}
                   handleAddOnsChange={handleAddOnsChange}
-                  add_ons={checkBoxStatus}
+                  add_ons={add_ons_selected}
                   />}
         />
         <Route path='/step4' 
